eeafd58c6122b6889b80fe9f86b1556363ad1250
rodrigo pichardo <hectorrodrigo2017@gmail.com>
2021-10-27 11:06:17 -0500

    docs agregado

---
+++ docs/precache-manifest.abe6b8eb15ee5d7c5003390714568f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37470b9300011b6067c16e5cc0a8555b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2b7ee5fa1ec30b9b4cf",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b7ee5fa1ec30b9b4cf",
+    "url": "/static/js/main.55d288ff.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
